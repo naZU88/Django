a81b57c062aa0f55e7ed91ea2b9372d0c51bb16c
Yana <greedygoga@gmail.com>
2024-04-07 19:41:21 +0300

    Added hw5

---
+++ djcourse/fifthapp/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+@admin.action(description="Сбросить количество в ноль")
+def reset_quantity(modeladmin, request, queryset):
+    queryset.update(quantity=0)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'quantity']
+    ordering = ['category', '-quantity']
+    list_filter = ['date_added', 'price']
+    search_fields = ['description']
+    search_help_text = 'Поиск по полю Описание продукта (description)'
+    actions = [reset_quantity]
+    
+    fields = ['name', 'description', 'category', 'date_added', 'rating']
+    readonly_fields = ['date_added', 'rating']
+
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
--- djcourse/secondapp/admin.py
+++ djcourse/secondapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Product, Order, Client
 
-# Register your models here.
+
+@admin.action(description="Сбросить количество в ноль")
+def reset_quant(modeladmin, request, queryset):
+    queryset.update(quant=0)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'price', 'quant']
+    ordering = ['-price']
+    list_filter = ['date_add', 'price']
+    search_fields = ['description']
+    search_help_text = 'Поиск по полю Описание продукта (description)'
+    actions = [reset_quant]
+    
+    fields = ['name', 'description', 'quant', 'date_add', 'price']
+    readonly_fields = ['date_add']
+
+
+admin.site.register(Client)
+admin.site.register(Order)
+admin.site.register(Product, ProductAdmin)
